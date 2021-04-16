0ce374bb05f0ae4537fc505701f90af9535cc993
cnzaicom <cnzaicom@users.noreply.github.com>
2021-04-16 21:42:00 +0800

    Add files via upload

---
+++ TailwindCssPreset.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Cnzaicom\TailwindCssPreset;
+
+use Illuminate\Container\Container;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Laravel\Ui\Presets\Preset;
+use Symfony\Component\Finder\SplFileInfo;
+
+class TailwindCssPreset extends Preset
+{
+    public static function install()
+    {
+        static::updatePackages();
+        static::updateWelcomePage();
+        static::removeNodeModules();
+    }
+
+    public static function installAuth()
+    {
+        static::scaffoldController();
+        static::scaffoldAuth();
+    }
+
+    protected static function updatePackageArray(array $packages)
+    {
+        return array_merge([
+            'laravel-mix' => '^6.0.16',
+            '@tailwindcss/custom-forms'=> '^0.2.1',
+            'tailwindcss' => '^2.1.1',
+        ], Arr::except($packages, [
+            'bootstrap',
+            'laravel-mix',
+        ]));
+    }
+
+
+    protected static function updateWelcomePage()
+    {
+        (new Filesystem)->delete(resource_path('views/welcome.blade.php'));
+
+        copy(__DIR__.'/tailwindcss-stubs/resources/views/welcome.blade.php', resource_path('views/welcome.blade.php'));
+    }
+
+    protected static function scaffoldController()
+    {
+        if (! is_dir($directory = app_path('Http/Controllers/Auth'))) {
+            mkdir($directory, 0755, true);
+        }
+
+        $filesystem = new Filesystem;
+
+        collect($filesystem->allFiles(base_path('vendor/laravel/ui/stubs/Auth')))
+            ->each(function (SplFileInfo $file) use ($filesystem) {
+                $filesystem->copy(
+                    $file->getPathname(),
+                    app_path('Http/Controllers/Auth/'.Str::replaceLast('.stub', '.php', $file->getFilename()))
+                );
+            });
+    }
+
+    protected static function scaffoldAuth()
+    {
+        file_put_contents(app_path('Http/Controllers/HomeController.php'), static::compileControllerStub());
+
+        file_put_contents(
+            base_path('routes/web.php'),
+            "Auth::routes();\n\nRoute::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');\n\n",
+            FILE_APPEND
+        );
+
+        tap(new Filesystem, function ($filesystem) {
+            $filesystem->copyDirectory(__DIR__.'/tailwindcss-stubs/resources/views', resource_path('views'));
+
+            collect($filesystem->allFiles(base_path('vendor/laravel/ui/stubs/migrations')))
+                ->each(function (SplFileInfo $file) use ($filesystem) {
+                    $filesystem->copy(
+                        $file->getPathname(),
+                        database_path('migrations/'.$file->getFilename())
+                    );
+                });
+        });
+    }
+
+    protected static function compileControllerStub()
+    {
+        return str_replace(
+            '{{namespace}}',
+            Container::getInstance()->getNamespace(),
+            file_get_contents(__DIR__.'/tailwindcss-stubs/controllers/HomeController.stub')
+        );
+    }
+}
