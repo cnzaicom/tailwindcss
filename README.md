13ebe428e9c32a1f89a3c75b35bfee4b781d8695
magic <cnzaicoma@126.com>
2021-04-17 15:47:59 +0800

    modify4

--- README.md
+++ README.md
@@ -12,6 +12,11 @@
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
+        /*自动增加 CSS 前缀词*/
+        require("autoprefixer"),
+        /*在 CSS 用嵌套的写法*/
+        require('postcss-nested'),
+        /*在 CSS 里可以 import 其他 CSS*/
         require('postcss-import'),
     ]);
 ```
--- src/TailwindCssPreset.php
+++ src/TailwindCssPreset.php
@@ -31,6 +31,8 @@ class TailwindCssPreset extends Preset
             'tailwindcss' => '^2.0.1',
             "@tailwindcss/forms"=> "^0.3.2",
             "@tailwindcss/typography"=> "^0.4.0",
+            "postcss-import" => "^14.0.0",
+            "postcss-nested" => "^5.0.0",
         ], Arr::except($packages, [
             'bootstrap',
             'laravel-mix',
